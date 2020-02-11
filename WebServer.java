8c60c3965583791952ee7a8aa7c6989091100d65
Mohamed Farah <47758699+FarahMo24@users.noreply.github.com>
2020-02-10 17:30:10 -0800

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -5,9 +5,12 @@ import java.io.*;
 public class WebServer {
 
 	private ServerConfiguration config;
+	private ServerMimeMap mimeType;
 
 	public WebServer() {
 		this.config = new ServerConfiguration(new File("./conf/httpd.conf"));
+		this.mimeType = new ServerMimeMap(new File("./conf/mime.types"));
+		
 	}
 
 	public static void main(String[] args) {
